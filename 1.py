9ddb028fde65a38eb1f8cbbfec79d2171d146576
haryaugust <bullsbulls24@gmail.com>
2019-11-22 18:24:15 +0700

    ubah isi text file master agar tidak terjadi bentrok dengan cabang, isi text yang sama

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
 print ("Hello, Hary Agustian")
 print ("Hello, Selamat Datang di Arkademy")
 print ("Hello, Saya Sedang Belajar GIT")
+print ("AGAR TIDAK TERJADI BENTROK DENGAN CARA MERUBAH ISI FILE")
